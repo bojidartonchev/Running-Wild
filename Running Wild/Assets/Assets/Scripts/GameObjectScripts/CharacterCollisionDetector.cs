38497de972de611cbb3820d82e79554e85b83dea
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2016-03-25 02:15:33 +0200

    Enemy improvements/Character improvements:
    -Horse model changed to Dinosaur.
    -Collisions for switching enemy's animation added.
    -Collisions for player death added.
    
    -Character hands added(animated).

---
+++ Running Wild/Assets/Assets/Scripts/GameObjectScripts/CharacterCollisionDetector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CharacterCollisionDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Animal")
+        {
+            collision.gameObject.GetComponent<Animation>().Play("Attack");
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Running Wild/Assets/Assets/Scripts/Utilities/SetKinematic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetKinematic : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Terrain")
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+}
