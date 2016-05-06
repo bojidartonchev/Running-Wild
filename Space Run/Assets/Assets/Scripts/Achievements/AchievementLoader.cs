3e7fd24c17f5f5929cf8a4ae9108b228f505981c
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2016-05-06 22:10:52 +0300

    Notifications service added. Achievements fixed

---
+++ Space Run/Assets/Assets/Scripts/Achievements/AchievementLoader.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Achievement;
+using System.Collections.Generic;
+using Assets.Assets.Scripts.UI;
+
+public class AchievementLoader : MonoBehaviour {
+
+    public GameObject player;
+    
+    private List<AchievementVariable<int>> distance;
+    private AchievementDefinition completedAchievement = null;
+
+    // Use this for initialization
+    void Start () {
+        
+        distance = new List<AchievementVariable<int>>();
+        distance.Add(new AchievementVariable<int>("100"));
+        distance.Add(new AchievementVariable<int>("250"));
+        distance.Add(new AchievementVariable<int>("500"));
+        distance.Add(new AchievementVariable<int>("1250"));
+        distance.Add(new AchievementVariable<int>("2500"));
+        
+        AchievementManager.Instance.onComplete += AchievementComplete;
+
+        foreach (AchievementDefinition def in AchievementManager.Instance.Definitions)
+        {
+            if (PlayerPrefs.GetInt(def.name, 0) == 1)
+                AchievementManager.Instance.MarkCompleted(def.name);
+        }
+        //deletes achievement history
+        PlayerPrefs.DeleteAll();
+        AchievementManager.Instance.Clear();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(AchievementVariable<int> dist in distance)
+        {
+            if (dist.Value <= int.Parse(dist.identifier)){
+                dist.Value = (int)player.GetComponent<DistanceCounter>().Distance;
+            }
+        }
+    }
+
+    void AchievementComplete(AchievementDefinition def)
+    {
+        if (PlayerPrefs.GetInt(def.name) != 1)
+        {
+            //notifing
+            Notifier.Instance.Notify(def.title);
+            
+            //saving
+            PlayerPrefs.SetInt(def.name, 1);
+            PlayerPrefs.Save();
+        }
+    }
+}
---
+++ Space Run/Assets/Assets/Scripts/Notifier/Notifier.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Notifier : MonoBehaviour {
+
+    private static Notifier instance;
+    private GameObject notifierWindow;
+    private float visibilityTime = 2f;
+    private Queue<string> notifications = new Queue<string>();
+    private Vector2 boxPossition = new Vector2(600, 200);
+    private bool showingNotification;
+    private string messageToShow;
+    
+
+    public static Notifier Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject fbm = new GameObject("Notifier");
+                fbm.AddComponent<Notifier>();
+            }
+            return instance;
+        }
+    }
+    private GameObject NotifierWindow
+    {
+        get
+        {
+            if (notifierWindow == null)
+            {
+                notifierWindow = GameObject.Find("NotifierWindow");
+            }
+            return notifierWindow;
+        }
+    }
+
+    public void Notify(string message)
+    {
+        notifications.Enqueue(message);
+        if (!showingNotification)
+        {
+            StartCoroutine(ShowNotificationBox());
+        }
+    }
+
+    IEnumerator ShowNotificationBox()
+    {
+        this.showingNotification = true;
+
+        while (showingNotification)
+        {
+            this.messageToShow = notifications.Dequeue();
+            // show box
+            yield return StartCoroutine(AnimateBox());
+            if (notifications.Count == 0)
+            {
+                showingNotification = false;
+            }
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        instance = this;
+    }
+
+    IEnumerator AnimateBox()
+    {
+        // come in
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            boxPossition.y = Mathf.Lerp(-200, 100, t);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(this.visibilityTime);
+
+        // go out
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            boxPossition.y = Mathf.Lerp(-200, 100, t);
+            yield return null;
+        }
+    }
+    void OnGUI()
+    {
+        if (showingNotification)
+        {
+            this.NotifierWindow.transform.position = new Vector2(this.NotifierWindow.transform.position.x, boxPossition.y);
+            this.NotifierWindow.transform.Find("Title").GetComponent<Text>().text = this.messageToShow;
+        }
+    }
+}
